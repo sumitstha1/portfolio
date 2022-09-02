3c47785ee7fa546c4651f320780cec307f6bb483
sixdgt <edchristian.inventor@gmail.com>
2022-09-02 23:19:30 +0545

    created new portfolio site

---
+++ portfolio/app_portfolio/views.py
@@ -0,0 +1,12 @@
+from encodings import search_function
+from re import template
+from django.shortcuts import render
+
+# Create your views here.
+def start_page(request):
+    template = 'portfolio/starting.html'
+    return render(request, template)
+
+def port_index(request):
+    template = 'portfolio/index.html'
+    return render(request, template)
