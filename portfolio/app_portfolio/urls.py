3dcbc8402da54ef802050dbe8f8d64bbb1c33ea1
sixdgt <edchristian.inventor@gmail.com>
2022-09-04 04:11:02 +0545

    added sign up & login button and design with css

--- portfolio/app_portfolio/urls.py
+++ portfolio/app_portfolio/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # user login
+    path('login/', views.login_page, name='login'),
+    path('register/', views.register_page, name='register'),
+    
+
+    # pages
     path('', views.start_page, name='starting'),
     path('index/', views.port_index, name='index'),
     path('contact/', views.contact_page, name='contact'),
--- portfolio/app_portfolio/views.py
+++ portfolio/app_portfolio/views.py
@@ -3,6 +3,15 @@ from re import template
 from django.shortcuts import render
 
 # Create your views here.
+def login_page(request):
+    template = 'users/login.html'
+    return render(request, template)
+
+def register_page(request):
+    template = 'users/register.html'
+    return render(request, template)
+
+
 def start_page(request):
     template = 'portfolio/starting.html'
     return render(request, template)
