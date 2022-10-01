4e6f1948971f07a833be6733dae2219289331e26
sixdgt <edchristian.inventor@gmail.com>
2022-10-01 22:50:29 +0545

    host

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'sumitsporfolio12.herokuapp.com']
 
 
 # Application definition
