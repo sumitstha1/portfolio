1e353b28f4eb3460aef09a646679f49af9ca80db
sixdgt <edchristian.inventor@gmail.com>
2022-10-01 21:58:26 +0545

    edited

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,3 +145,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config("EADDRESS")
 EMAIL_HOST_PASSWORD = config("EPASSWORD")
+
+
+# Activate Django Heroku
+django_heroku.settings(locals())
