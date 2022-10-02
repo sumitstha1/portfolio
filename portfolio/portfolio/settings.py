fd6ca2efad937a20699748e652b5017885d850c5
sixdgt <edchristian.inventor@gmail.com>
2022-10-02 12:46:51 +0545

    added

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-)41_rokmg^rd6%(8scwi7!ungt9!@t*)+q#j_mh3id-1loxr^-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'sumitsporfolio12.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'sumitportfolio12.herokuapp.com']
 
 
 # Application definition
@@ -128,10 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_HOST = "https://d4663kmspf1sqa.cloudfront.net" if not DEBUG else ""
+# STATIC_HOST = "https://d4663kmspf1sqa.cloudfront.net" if not DEBUG else ""
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR), 'static/']
-STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 
